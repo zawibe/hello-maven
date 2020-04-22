ce6f7013e05d075792c901ca8cae45c0673b9872
Wojciech Frącz <fraczwojciech@gmail.com>
2020-04-22 21:56:25 +0200

    Hello Maven with java-ascii-render

--- src/main/java/pl/edu/agh/mwo/hellomaven/App.java
+++ src/main/java/pl/edu/agh/mwo/hellomaven/App.java
@@ -1,12 +1,27 @@
 package pl.edu.agh.mwo.hellomaven;
 
+import com.indvd00m.ascii.render.Render;
+import com.indvd00m.ascii.render.api.ICanvas;
+import com.indvd00m.ascii.render.api.IContextBuilder;
+import com.indvd00m.ascii.render.api.IRender;
+import com.indvd00m.ascii.render.elements.PseudoText;
+
 /**
  * Simple main application.
  */
 public class App {
 
+    private static final int TEXT_WIDTH = 120;
+    private static final int TEXT_HEIGHT = 20;
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        IRender render = new Render();
+        IContextBuilder builder = render.newBuilder();
+        builder.width(TEXT_WIDTH).height(TEXT_HEIGHT);
+        builder.element(new PseudoText("Hello Maven!"));
+        ICanvas canvas = render.render(builder.build());
+        String s = canvas.getText();
+        System.out.println(s);
     }
 
 }
